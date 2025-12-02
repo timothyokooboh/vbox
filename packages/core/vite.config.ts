afbf481ebd5036acc0db1a602f1378fe4bb8d8f7
timothy okooboh <okoobohtimothy@gmail.com>
2025-12-02 20:19:34 +0100

    chore: files formatting

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,13 +9,13 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [
     dts({
-      outDir: "dist/types",
+      outDir: 'dist/types',
     }),
   ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "VBoxCore",
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'VBoxCore',
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
