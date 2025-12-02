6dbeae9bdbabf2d0fb17f070cfdb0c43d69f00bd
timothy okooboh <okoobohtimothy@gmail.com>
2025-12-02 18:58:13 +0100

    feat: delegate creation of defineConfig to the core module

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,13 +1,7 @@
 import type VBox from "./components/VBox.vue";
-import type { VBoxPluginOptions } from "./plugins";
 
 export { default as VBox } from "./components/VBox.vue";
 export { VBoxPlugin } from "./plugins";
-export type { VBoxPluginOptions, AliasStrategy } from "./plugins";
-
-export function defineConfig(config: VBoxPluginOptions) {
-  return config;
-}
 
 declare module "vue" {
   export interface GlobalComponents {
