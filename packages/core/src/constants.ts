68c3fbbf71ef72248b37160f761fde9430e1e765
timothy okooboh <okoobohtimothy@gmail.com>
2025-12-02 20:13:10 +0100

    feat: add turbo repo

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,49 +1,49 @@
-import type { Breakpoints } from "./types";
+import type { Breakpoints } from './types';
 
 export const DefaultAliases = {
-  w: "width",
-  maxW: "maxWidth",
-  minW: "minWidth",
-  h: "height",
-  maxH: "maxHeight",
-  minH: "minHeight",
-  fs: "fontSize",
-  fw: "fontWeight",
-  ff: "fontFamily",
-  lh: "lineHeight",
-  ls: "letterSpacing",
-  m: "margin",
-  mt: "marginBlockStart",
-  mb: "marginBlockEnd",
-  ml: "marginInlineStart",
-  mr: "marginInlineEnd",
-  mx: "marginInline",
-  my: "marginBlock",
-  p: "padding",
-  pt: "paddingBlockStart",
-  pb: "paddingBlockEnd",
-  pl: "paddingInlineStart",
-  pr: "paddingInlineEnd",
-  px: "paddingInline",
-  py: "paddingBlock",
-  bg: "background",
-  bgColor: "backgroundColor",
-  bgSize: "backgroundSize",
-  bgPosition: "backgroundPosition",
-  bgImage: "backgroundImage",
-  bgClip: "backgroundClip",
-  bgRepeat: "backgroundRepeat",
-  bgAttachment: "backgroundAttachment",
-  bgOrigin: "backgroundOrigin",
-  radius: "borderRadius",
-  shadow: "boxShadow",
-  d: "display",
+  w: 'width',
+  maxW: 'maxWidth',
+  minW: 'minWidth',
+  h: 'height',
+  maxH: 'maxHeight',
+  minH: 'minHeight',
+  fs: 'fontSize',
+  fw: 'fontWeight',
+  ff: 'fontFamily',
+  lh: 'lineHeight',
+  ls: 'letterSpacing',
+  m: 'margin',
+  mt: 'marginBlockStart',
+  mb: 'marginBlockEnd',
+  ml: 'marginInlineStart',
+  mr: 'marginInlineEnd',
+  mx: 'marginInline',
+  my: 'marginBlock',
+  p: 'padding',
+  pt: 'paddingBlockStart',
+  pb: 'paddingBlockEnd',
+  pl: 'paddingInlineStart',
+  pr: 'paddingInlineEnd',
+  px: 'paddingInline',
+  py: 'paddingBlock',
+  bg: 'background',
+  bgColor: 'backgroundColor',
+  bgSize: 'backgroundSize',
+  bgPosition: 'backgroundPosition',
+  bgImage: 'backgroundImage',
+  bgClip: 'backgroundClip',
+  bgRepeat: 'backgroundRepeat',
+  bgAttachment: 'backgroundAttachment',
+  bgOrigin: 'backgroundOrigin',
+  radius: 'borderRadius',
+  shadow: 'boxShadow',
+  d: 'display',
 } as const;
 
 export const DefaultBreakpoints: Breakpoints = {
-  sm: "40rem", // ~640px
-  md: "48rem", // ~768px
-  lg: "64rem", // ~1024px
-  xl: "80rem", // ~1280px
+  sm: '40rem', // ~640px
+  md: '48rem', // ~768px
+  lg: '64rem', // ~1024px
+  xl: '80rem', // ~1280px
   //"2xl": "96rem", // ~1536px
 };
