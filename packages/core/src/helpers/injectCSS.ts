928aed8ef3e14c9eb73fe6c22d870cd4ba68e873
timothy okooboh <okoobohtimothy@gmail.com>
2025-12-06 19:38:46 +0100

    feat: implement autoprefixing

--- packages/core/src/helpers/injectCSS.ts
+++ packages/core/src/helpers/injectCSS.ts
@@ -1,3 +1,19 @@
+import { compile, serialize, stringify, middleware, prefixer } from 'stylis';
+
+const cache = new Map<string, string>();
+
+const createVendoPrefix = (css: string) => {
+  if (cache.has(css)) return cache.get(css);
+
+  const prefixedCss = serialize(
+    compile(css),
+    middleware([prefixer, stringify]),
+  );
+
+  cache.set(css, prefixedCss);
+  return prefixedCss;
+};
+
 export const injectCSS = (css: string) => {
   if (typeof window === 'undefined') return;
 
@@ -5,12 +21,14 @@ export const injectCSS = (css: string) => {
   let styleEl = document.getElementById(id) as HTMLStyleElement | null;
 
   if (styleEl) {
-    styleEl.textContent += css;
+    const prefixedCss = createVendoPrefix(css);
+    styleEl.textContent += prefixedCss;
     return;
   }
 
   styleEl = document.createElement('style');
   styleEl.id = id;
-  styleEl.textContent += css;
+  const prefixedCss = createVendoPrefix(css);
+  styleEl.textContent += prefixedCss;
   document.head.appendChild(styleEl);
 };
