abbc1be846588086789a59f47b441d8a96017d5a
timothy okooboh <okoobohtimothy@gmail.com>
2025-12-06 09:08:27 +0100

    fix: use a valid id attribute for vbox style element

--- packages/core/src/helpers/injectCSS.ts
+++ packages/core/src/helpers/injectCSS.ts
@@ -1,7 +1,7 @@
 export const injectCSS = (css: string) => {
-  if (typeof window === "undefined") return;
+  if (typeof window === 'undefined') return;
 
-  const id = "@vbox-style-sheet";
+  const id = 'vbox-style-sheet';
   let styleEl = document.getElementById(id) as HTMLStyleElement | null;
 
   if (styleEl) {
@@ -9,7 +9,7 @@ export const injectCSS = (css: string) => {
     return;
   }
 
-  styleEl = document.createElement("style");
+  styleEl = document.createElement('style');
   styleEl.id = id;
   styleEl.textContent += css;
   document.head.appendChild(styleEl);
