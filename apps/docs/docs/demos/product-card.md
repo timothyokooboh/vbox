1887ebe41de2ed16aaaf4442fd1db2f8711b3cde
timothy okooboh <okoobohtimothy@gmail.com>
2025-12-19 09:32:09 +0100

    chore: update docs

--- apps/docs/docs/demos/product-card.md
+++ apps/docs/docs/demos/product-card.md
@@ -6,7 +6,9 @@ This product card is styled 100% with VBox :tada:
     <ProductCard />
 </ClientOnly>
 
-[Source code](https://github.com/timothyokooboh/vbox/blob/main/apps/docs/docs/examples/demos/product-card/ProductCard.vue)
+<<< @/examples/demos/product-card/ProductCard.vue
+
+[Full source code](https://github.com/timothyokooboh/vbox/blob/main/apps/docs/docs/examples/demos/product-card/ProductCard.vue)
 
 <script lang="ts" setup>
 import ProductCard from '../examples/demos/product-card/ProductCard.vue'
--- apps/docs/docs/examples/demos/product-card/ProductCard.vue
+++ apps/docs/docs/examples/demos/product-card/ProductCard.vue
@@ -9,28 +9,7 @@
     ff="ff-sans"
   >
     <ProductMedia />
-
-    <v-box
-      position="relative"
-      z="10"
-      mt="-1.5rem"
-      display="flex"
-      flex-direction="column"
-      gap="1rem"
-      border-top-right-radius="1.5rem"
-      border-top-left-radius="1.5rem"
-      px="0.6rem"
-      pt="2rem"
-      pb="1.5rem"
-      shadow="0 -10px 40px -15px rgba(0,0,0,0.3)"
-      bg="#fff"
-      :dark="{
-        bg: '#23220F',
-      }"
-      :md="{
-        px: '1.5rem',
-      }"
-    >
+    <ProductDetails>
       <ProductHeader />
       <ProductSizeSelector />
       <ProductDescription />
@@ -38,12 +17,13 @@
         <ProductQtySelector />
         <AddToCartBtn />
       </v-box>
-    </v-box>
+    </ProductDetails>
   </v-box>
 </template>
 
 <script setup lang="ts">
 import ProductMedia from './ProductMedia.vue';
+import ProductDetails from './ProductDetails.vue';
 import ProductHeader from './ProductHeader.vue';
 import ProductSizeSelector from './ProductSizeSelector.vue';
 import ProductDescription from './ProductDescription.vue';
